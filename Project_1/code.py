159ba8a7b334a5495dd7da5e05f489d61ada820a
K Samarth N Kamath <kamathsamarth@gmail.com>
2020-05-04 13:48:34 +0530

    Update code.py

--- Project_1/code.py
+++ Project_1/code.py
@@ -91,10 +91,10 @@ def initialize_with_zeros(dim):
     b -- initialized scalar (corresponds to the bias)
     """
     
-    ### START CODE HERE ### (≈ 1 line of code)
+    
     w = np.zeros((dim,1))
     b = 0
-    ### END CODE HERE ###
+    
 
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
@@ -108,4 +108,54 @@ print ("w = " + str(w))
 print ("b = " + str(b))
 
 #Output_zero is output for above code
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    
+    A = sigmoid(np.dot(w.T, X) + b)                                     # compute activation
+    cost = -1./m* np.sum(Y*np.log(A) + (1-Y)*np.log(1-A))                                  # compute cost
+    
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    
+    dw = 1./m*np.dot(X, (A-Y).T)
+    db = 1./m*np.sum(A-Y)
+    
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    #cost = np.squeeze(cost)
+    #assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+w, b, X, Y = np.array([[1],[2]]), 2, np.array([[1,2],[3,4]]), np.array([[1,0]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+#Output_Propagation gives the output of above code
 
