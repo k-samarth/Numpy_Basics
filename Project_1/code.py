c93f9e9c5d31e45b81905c6949eb6f3abb3c8f88
K Samarth N Kamath <kamathsamarth@gmail.com>
2020-05-04 13:55:53 +0530

    Update code.py

--- Project_1/code.py
+++ Project_1/code.py
@@ -202,7 +202,7 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
         
         w = w - learning_rate * dw
         b = b - learning_rate * db
-        ### END CODE HERE ###
+        
         
         # Record the costs
         if i % 100 == 0:
@@ -227,4 +227,43 @@ print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
 
 #Output_Optimization gives the output of above code
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    
+    A =   sigmoid(np.dot(w.T, X) + b)
+    
+    
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+       
+        if A[0, i] > 0.5:
+            Y_prediction[0, i] = 1
+        else:
+             Y_prediction[0, i] = 0
+        
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+print ("predictions = " + str(predict(w, b, X)))
+
+#Out_Predict gives output of above code
 
