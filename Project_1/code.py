42ea63733a9cd6a84f7a24543a3e0df55906ba49
K Samarth N Kamath <kamathsamarth@gmail.com>
2020-05-04 14:02:46 +0530

    Update code.py

--- Project_1/code.py
+++ Project_1/code.py
@@ -323,3 +323,8 @@ d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 200
 
 #Output_Model gives the output of above code
 
+index = 1
+plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
+
+#Output_Cat gives the output of above code
